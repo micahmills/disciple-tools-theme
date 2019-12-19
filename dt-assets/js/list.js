e97212e43610784da875afb064f814077189c0b7
Micah Mills <micahmills@gmail.com>
2019-12-19 23:08:11 +0300

    When the site was set to English there was no lang tag. This defaults the locale to en if there is no lang-tag in the html

--- dt-assets/js/list.js
+++ dt-assets/js/list.js
@@ -313,7 +313,14 @@
       return '<a href="' + _.escape(group.permalink) + '">' + group.post_title + "</a>";
     }).join(", ");
 
-    const last_modified = new Date(contact.last_modified*1000).toString().slice(0, 15);
+    const langTag = document.querySelector('html').getAttribute('lang')
+    if (langTag) {
+      var locale = langTag.replace("_", "-");
+    } else {
+      var locale = "en"
+    }
+
+    const last_modified = new Date(contact.last_modified*1000).toLocaleDateString(locale).slice(0, 15);
 
     const context = _.assign({last_modified: 0}, contact, wpApiListSettings, {
       index,
