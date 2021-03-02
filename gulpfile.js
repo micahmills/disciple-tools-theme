10cfd467d682603d7f01a77985449cd0fb31f710
Micah Mills <micahmills@gmail.com>
2021-03-02 11:30:16 +0300

    minify and uglify the js files

--- gulpfile.js
+++ gulpfile.js
@@ -123,6 +123,8 @@ gulp.task('scripts', function () {
 gulp.task('lodash', function(){
   return gulp.src(['dt-assets/**/*.js'])
      .pipe(replace('_.', 'window.lodash.'))
+     .pipe(plugin.uglify())
+     .pipe(rename({ suffix: '.min' }))
      .pipe(gulp.dest('dt-assets/build'));
 });
 
